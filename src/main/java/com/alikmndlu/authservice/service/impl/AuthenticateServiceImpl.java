296a733b0a3df8b949acef256e9f8fd4462bd921
Ali Kmndlu <alikmndlu1@gmail.com>
2022-01-15 18:14:28 +0330

    remove unused imports and replace @Data with Getter And Setter

--- src/main/java/com/alikmndlu/authservice/service/impl/AuthenticateServiceImpl.java
+++ src/main/java/com/alikmndlu/authservice/service/impl/AuthenticateServiceImpl.java
@@ -3,15 +3,10 @@ package com.alikmndlu.authservice.service.impl;
 import com.alikmndlu.authservice.dto.RegisterCredentialsDto;
 import com.alikmndlu.authservice.dto.UserDto;
 import com.alikmndlu.authservice.service.AuthenticateService;
-import com.alikmndlu.authservice.util.StaticUtils;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.server.ResponseStatusException;
 
 import java.util.Map;
 
