cdab907b538763d614f33d43687303f2ab200169
Ali Kmndlu <alikmndlu1@gmail.com>
2022-01-18 15:55:09 +0330

    add register api test

--- src/test/java/com/alikmndlu/authservice/AuthServiceApplicationTest.java
+++ src/test/java/com/alikmndlu/authservice/AuthServiceApplicationTest.java
@@ -1,5 +1,6 @@
 package com.alikmndlu.authservice;
 
+import com.github.javafaker.Faker;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -9,7 +10,7 @@ import org.springframework.http.ResponseEntity;
 
 import java.util.Map;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest(
         classes = AuthServiceApplication.class,
@@ -23,22 +24,14 @@ public class AuthServiceApplicationTest {
     @Autowired
     private TestRestTemplate restTemplate;
 
+    private final Faker faker = new Faker();
 
     @Test
-    void test() {
-
-        ResponseEntity<Map> loginResponse = restTemplate.postForEntity(
-                "http://localhost:" + port + "/api/auth/login",
-                Map.of(
-                        "emailAddress", "alikmndlu1@gmail.com",
-                        "password", "123"
-                ),
-                Map.class
-        );
-        assertTrue(loginResponse.getStatusCode().is2xxSuccessful());
+    void login(){
+        ResponseEntity<Map> loginResponse;
 
         loginResponse = restTemplate.postForEntity(
-                "http://localhost:" + port + "/api/auth/login",
+                "http://localhost:8080/api/auth/login",
                 Map.of(
                         "emailAddress", "alikmndlu1@gmail.com",
                         "password", "1234"
@@ -46,5 +39,31 @@ public class AuthServiceApplicationTest {
                 Map.class
         );
         assertTrue(loginResponse.getStatusCode().is4xxClientError());
+
+        loginResponse = restTemplate.postForEntity(
+                "http://localhost:8080/api/auth/login",
+                Map.of(
+                        "emailAddress", "alikmndlu1@gmail.com",
+                        "password", "123"
+                ),
+                Map.class
+        );;
+        assertTrue(loginResponse.getStatusCode().is2xxSuccessful());
+    }
+
+    @Test
+    void register(){
+        ResponseEntity<Map> registerResponse;
+
+        registerResponse = restTemplate.postForEntity(
+                "http://localhost:8080/api/auth/register",
+                Map.of(
+                        "name", faker.name().fullName(),
+                        "emailAddress", faker.internet().emailAddress(),
+                        "password", faker.internet().password()
+                ),
+                Map.class
+        );
+        assertTrue(registerResponse.getStatusCode().is2xxSuccessful());
     }
 }
